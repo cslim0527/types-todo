269db8a04dacddfa7f66e3c2dc26ed3c6494eca1
limchansoo <cslim0527@gmail.com>
2022-10-16 23:13:17 +0900

    type alias, enum 사용, MVC 패턴으로 기능분리

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
-import { data } from "./data";
-import TodoItem from "./TodoItem";
-import TodoCollection from "./TodoCollection";
+import TodoConsole from "./view/TodoConsole";
 
-const sampleTodos: TodoItem[] = data.map(
-  ({ id, task, complete }) => new TodoItem(id, task, complete)
-);
-
-const myTodoCollection = new TodoCollection("My Todo List", sampleTodos);
-
-myTodoCollection.addTodo("Javascript 학습하기");
-myTodoCollection.addTodo("밥 먹기");
-myTodoCollection.markComplete(3, true);
-myTodoCollection.removeComplete();
-
-console.log("-----", myTodoCollection.userName, "-----");
-myTodoCollection.getTodoItems(true).forEach((todo) => todo.printDetails());
-console.log("========================");
-myTodoCollection.getTodoItems(false).forEach((todo) => todo.printDetails());
+const todoConsole = new TodoConsole();
+todoConsole.promptUser();
